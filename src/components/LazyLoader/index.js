702aa3b821ac554f86cf611486c6aeffb6af3517
AleksanderKishchak <sanya69876@gmail.com>
2021-04-14 20:59:37 +0300

    fix flickering

--- src/components/LazyLoader/index.js
+++ src/components/LazyLoader/index.js
@@ -1,5 +1,7 @@
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Fade from '@material-ui/core/Fade';
+
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -13,8 +15,17 @@ export const LazyLoader = () => {
   const classes = useStyles();
 
   return (
-    <Backdrop className={classes.backdrop} open>
-      <CircularProgress color="inherit" />
-    </Backdrop>
+    <Fade
+      in
+      style={{
+        transitionDelay: '800ms',
+      }}
+      unmountOnExit
+    >
+      <Backdrop className={classes.backdrop} open>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </Fade>
+
   );
 };
--- src/provider/AppStateProvider.js
+++ src/provider/AppStateProvider.js
@@ -5,9 +5,7 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import { AppStateContext } from './AppStateContext';
 import { themeKeys, themes } from '../themes';
 
-export const AppStateProvider = ({ children }) => {
-  const defaultTheme = localStorage.getItem('theme') || themeKeys.light;
-  const [user, setUser] = useState({
+/* {
     id: 1,
     isAdmin: true,
     buildingId: [
@@ -21,9 +19,13 @@ export const AppStateProvider = ({ children }) => {
     status: {
       isPaid: true,
     },
-  }); // TODO change to null
+  } */
+
+export const AppStateProvider = ({ children }) => {
+  const defaultTheme = localStorage.getItem('theme') || themeKeys.light;
+  const [user, setUser] = useState(null); // TODO change to null
   // для двухфакторки временное решение
-  const [accessAllowed, setAccessAllowed] = useState(true); // TODO change to false
+  const [accessAllowed, setAccessAllowed] = useState(false); // TODO change to false
   const [theme, setTheme] = useState(defaultTheme);
 
   const value = {
