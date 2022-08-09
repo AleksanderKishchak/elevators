d3dc5474fc22697dcb4badb91934787bec8328b4
AleksanderKishchak <sanya69876@gmail.com>
2022-08-09 21:45:48 +0300

    777

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,2 +1,15 @@
-console.log('Hello from file', process.env.TEST)
-console.log(JSON.stringify(process.env.RELEASE))
+const releaseDescription = process.env.RELEASE;
+
+const taskRegExp = /https:\/\/pdffiller\.atlassian\.net\/browse\/CPU-\d{2,4}/g
+const uniq = (arr) => Array.from(new Set(arr))
+const getTaskIdFromUrl = (url) => url.split('/').pop()
+
+const getTasksIds = description => {
+  const tasksUrls = uniq(description.match(taskRegExp));
+
+  return tasksUrls.map(getTaskIdFromUrl)
+}
+
+const taskIds = getTasksIds(releaseDescription);
+
+console.log(taskIds)
